60983279de0647ba7df3ac59497c61aa39ba13d4
Håkon Anders Strømsodd <haakon8855@gmail.com>
2025-05-23 14:16:44 +0200

    Add DefaultAzureCredential for DB access in prod

--- NodeClient/src/services/dbService.ts
+++ NodeClient/src/services/dbService.ts
@@ -1,13 +1,39 @@
+import { DefaultAzureCredential } from '@azure/identity';
 import dotenv from 'dotenv';
 import sql from 'mssql';
+import logger from '../logger';
 
 dotenv.config();
 
 let pool: sql.ConnectionPool;
 
 const getPool = async () => {
+  logger.info('Getting pool');
   if (!pool) {
-    pool = await sql.connect(process.env.DB_CONNECTION_STRING!);
+    logger.info('Creating new pool');
+    if (process.env.NODE_ENV != 'prod') {
+      logger.info('Using connection string');
+      // Use connection string locally
+      pool = await sql.connect(process.env.DB_CONNECTION_STRING!);
+    } else {
+      logger.info('Using azure credential');
+      // Use managed identity in Azure (production)
+      const credential = new DefaultAzureCredential();
+      const { token } = await credential.getToken('https://database.windows.net');
+      pool = await sql.connect({
+        server: process.env.DB_SERVER!,
+        database: process.env.DB_NAME!,
+        authentication: {
+          type: 'azure-active-directory-access-token',
+          options: { token },
+        },
+        options: {
+          encrypt: true,
+        },
+      });
+    }
+  } else {
+    logger.info('Using existing pool');
   }
   return pool;
 };
